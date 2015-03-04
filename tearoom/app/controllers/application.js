7b02d56170ebd92d4f2a90b476ca5859a1c12bd6
Antony Church <achurch44.ac@gmail.com>
2015-03-04 20:01:33 +0000

    Implemented some routing ready for the highscores page and fixed a bug with the reset button

--- tearoom/app/controllers/application.js
+++ tearoom/app/controllers/application.js
@@ -1,35 +1,7 @@
 import Ember from 'ember';
 
 var ApplicationController = Ember.Controller.extend({
-    selectedBrewer: null,
-    brewerSelected: false,
-    actions:{
-	selectBrewer: function(){
-	    var allBrewers = this.get('model');
-	    var potentialBrewers = [];
-
-	    for(var i = 0; i < allBrewers.content.length; ++i){
-		var currentBrewer = allBrewers.content[i];
-		if(currentBrewer.get('inRound')){
-		    potentialBrewers.push(currentBrewer);
-		}
-	    }
-	    
-	    //Get random number between 0 and potential brewers length
-	    var selectedBrewerId = Math.floor(Math.random() * (potentialBrewers.length -1));
-	    this.set('selectedBrewer', potentialBrewers[selectedBrewerId]);
-	    this.set('brewerSelected', true);
-	},
-	reset: function(){
-	    var allBrewers = this.get('model');
-
-	    for(var i = 0; i < allBrewers.content.length; ++i){
-		var currentBrewer = allBrewers.content[i];
-		currentBrewer.set('inRound', false);
-		currentBrewer.save();
-	    }
-	}
-    }
+    
 });
 
 export default ApplicationController;
